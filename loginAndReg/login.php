cd576bac25c94e12342b52787d11f0f58f41d0f0
jlynli <130990818+jlynli@users.noreply.github.com>
2025-12-01 16:24:21 -0500

    code from class

--- loginAndReg/login.php
+++ loginAndReg/login.php
@@ -6,5 +6,12 @@ connect to DB
 query users.db for username and password
 if match, select 
 */
-    $path= "/home/jsl10027/loginAndReg/users.db";
+
+// step 1: we need to identify the full file path of where our
+// databases are stored on the server.
+$path = '/your/file/path/goes/here';
+
+// step 2: connect to our database, making sure to use the full
+// file path when specifying the database file location
+$db = new SQLite3($path.'/users.db');
 ?>
--- loginAndReg/register.php
+++ loginAndReg/register.php
@@ -1,3 +1,10 @@
 <?php
 //PHP HERE
+// step 1: we need to identify the full file path of where our
+// databases are stored on the server.
+$path = '/your/file/path/goes/here';
+
+// step 2: connect to our database, making sure to use the full
+// file path when specifying the database file location
+$db = new SQLite3($path.'/users.db');
 ?>
