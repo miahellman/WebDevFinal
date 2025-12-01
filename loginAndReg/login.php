b1d953a163e86d611a80988b9762499028f1995c
jlynli <130990818+jlynli@users.noreply.github.com>
2025-12-01 16:54:58 -0500

    inclass login template

--- loginAndReg/login.php
+++ loginAndReg/login.php
@@ -1,17 +1,137 @@
 <?php
-//PHP HERE
 
-/*
-connect to DB
-query users.db for username and password
-if match, select 
-*/
 
-// step 1: we need to identify the full file path of where our
-// databases are stored on the server.
-$path = '/your/file/path/goes/here';
+try {
+    // Open a connection to the SQLite database
+    // If the users.db doesn't exist, it will be created.
 
-// step 2: connect to our database, making sure to use the full
-// file path when specifying the database file location
-$db = new SQLite3($path.'/users.db');
+    // Use these 2 line when you run the php  live on i6 server  (the following two lines)
+    $path= "/home/jsl10027/databases";
+    $db = new SQLite3($path. '/users.db' );
+
+
+    //Comment the above code if you are testing the code for local server code ( the line after these comments)
+
+    // use this statemnt if testing from local server (and remove //). This will create a local users.db isnide the same folder as program
+    //     $db = new SQLite3($path. '/users.db' );
+
+    // report a message if all went fine for testing purposes
+    echo "Successfully connected to the users.db <br>";
+} 
+
+    // report the error if we couldnt open the test2.db  for testing purposes
+
+    catch (Exception $e) {
+    echo "Error connecting to the database: " . $e->getMessage() . "<br>";
+
+    exit();
+}
+
+
+// get values from form to to lokup in table and see if there is a match 
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+// print only for testing purposes and then remove later
+echo "<p> user entered value from form is $user and passowrd value entered from form is $password <p>";
+
+
+
+// create a line of code to lockup info from table users (was created inside the regSQL.php registration program)
+
+
+// not needed for this login example- you can remove
+// this is to demo how you get all info (all records or row from table)
+
+$sqlSelect = "SELECT id, user, password FROM users";
+
+
+// use try block to execute the query to query all fields from table users
+
+try {
+    $results = $db->query($sqlSelect);
+
+
+    // create formatted html output to print as output of query to user
+    // Agin, this is not needed for this example
+    // this is to demo how to print all rows from table
+    
+    echo "<h2>Users List looked up (search query) from Users table:</h2>";
+    echo "<ul>";
+
+
+    $flag = 0;
+    while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+        echo "<li> ID: " . $row['id'] . ", user: " . $row['user'] . ", password: " . $row['password'] . "</li>";
+    }
+ 
+
+} 
+
+// report errors based on executing above statement
+
+catch (Exception $e) {
+    echo "<p>Error querying data: " . $e->getMessage() . "<br>";
+}
+
+
+// find a match with user name and password from db and form values
+
+// create a line of code to lockup info from table users (was created inside the regSQL.php registration program)
+
+$sqlSelect2 = "SELECT id, user, password FROM users WHERE user='$user' and password='$password' ";
+
+
+// use try block to execute the query; 
+// See if you find a match
+
+try {
+
+    $results2 = $db->query($sqlSelect2);
+
+    if ($results2 === false) 
+    {
+        echo "<p>Error querying data: " . $db->lastErrorMsg() . "<br>";
+    } 
+
+    else 
+    {
+        // Try to fetch one row
+        $row = $results2->fetchArray(SQLITE3_ASSOC);
+
+        // if a it returns a row (a match found)
+        if ($row) 
+        {
+            // Match found
+            echo "<p>match found- Congrats!
+                <p> You are able to login.  Welcome to our website, $user!<br></p>";
+        } 
+
+        // no match found
+        else 
+        {
+            // No match
+            echo "<p>You can't access our website:<br>Wrong username or password. 
+                  <a href='login.html'>Try again</a></p>";
+        }
+    
+    }
+
+} 
+
+
+// report errors based on executing above statement
+
+catch (Exception $e) 
+{
+    echo "<p>Error querying data: " . $e->getMessage() . "<br>";
+}
+
+
+
+// close db
+
+$db->close();
+
+
 ?>
--- loginAndReg/register.php
+++ loginAndReg/register.php
@@ -1,10 +1,213 @@
 <?php
-//PHP HERE
-// step 1: we need to identify the full file path of where our
-// databases are stored on the server.
-$path = '/your/file/path/goes/here';
 
-// step 2: connect to our database, making sure to use the full
-// file path when specifying the database file location
-$db = new SQLite3($path.'/users.db');
+// use the headers for html from head.php file
+include "head.php";
+// use try block to catch and report errors when creating the db
+
+try {
+    // Open a connection to the SQLite database
+    // If the users.db doesn't exist, it will be created.
+
+    // Use these 2 line when you run the php  live on i6 server  (the following two lines)
+    $path= "/home/jsl10027/databases";
+    $db = new SQLite3($path. '/users.db' );
+
+
+    //Comment the above code if you are testing the code for local server code ( the line after these comments)
+
+    // use this statemnt if testing from local server (and remove //). This will create a local users.db isnide the same folder as program
+    //     $db = new SQLite3($path. '/users.db' );
+
+    // report a message if all went fine for testing purposes
+    echo "Successfully connected to the users.db <br>";
+} 
+
+    // report the error if we couldnt open the test2.db  for testing purposes
+
+    catch (Exception $e) {
+    echo "Error connecting to the database: " . $e->getMessage() . "<br>";
+
+    exit();
+}
+
+
+// create a table inside the test2.db; 
+//you can create as many tables as you need - for example you can create two tables for your final project: products and users
+
+
+
+// construct a create table (called users) query; if exists then igonre
+$sqlCreateTable = " CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT NOT NULL,
+        password TEXT  NOT NULL
+    );
+";
+
+// execute the line of code to create the table within try block to cath errors assoicated with executing this statemenet 
+
+try {
+    $db->exec($sqlCreateTable);
+    echo "Table 'users' created successfully or already exists.<br>";
+} 
+
+// catch the errors associated with excuting the above statement
+
+
+catch (Exception $e) {
+    echo "Error creating table: " . $e->getMessage() . "<br>";
+}
+
+// get values from form to insert inside the users table
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+echo "<p> $user: $password (only for testing purposes)<p>";
+
+
+// construct an insert query to store values inside table
+$sqlInsert = "INSERT INTO users (user, password) VALUES (:user, :password)";
+
+
+// use try block to construct an insert query  to store values into users.db
+
+try {
+
+    $stmt = $db->prepare($sqlInsert);
+    $stmt->bindValue(':user', $user, SQLITE3_TEXT);
+    $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+    
+    // execute the statement 
+    $stmt->execute();
+    echo " Info iserted: $user and $password values inserted successfully.<br>";
+
+
+} 
+
+// catch and report errors associated with executing above statements
+catch (Exception $e) {
+    echo "Error inserting data: " . $e->getMessage() . "<br>";
+}
+
+
+// if we are done with the database we should close it
+// this allows Apache to use it again quickly, rather than waiting for
+// the database's natural timeout to occur
+
+$db->close();
+unset($db);
+
+
+echo "Database connection closed.<br>";
+?>
+
+
+
+Using a complete  a SELECT Query with Try/catch block to catch errors and report it
+
+This is a complete example to Search/lockup values using SELECT Query  for login php program
+
+<?php
+
+
+// use try block to catch and report errors when creating the db
+
+try {
+    // Open a connection to the SQLite database
+    // If the users.db doesn't exist, it will be created.
+
+    // Use these 2 line when you run the php  live on i6 server  (the following two lines)
+    $path= "/home/sao1/databases";
+    $db = new SQLite3($path. '/users.db' );
+
+
+    //Comment the above code if you are testing the code for local server code ( the line after these comments)
+
+    // use this statemnt if testing from local server (and remove //). This will create a local users.db isnide the same folder as program
+    //     $db = new SQLite3($path. '/users.db' );
+
+    // report a message if all went fine for testing purposes
+    echo "Successfully connected to the users.db <br>";
+} 
+
+    // report the error if we couldnt open the test2.db  for testing purposes
+
+    catch (Exception $e) {
+    echo "Error connecting to the database: " . $e->getMessage() . "<br>";
+
+    exit();
+}
+
+
+// create a table inside the test2.db; 
+//you can create as many tables as you need - for example you can create two tables for your final project: products and users
+
+
+
+// construct a create table (called users) query; if exists then igonre
+$sqlCreateTable = " CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT NOT NULL,
+        password TEXT  NOT NULL
+    );
+";
+
+// execute the line of code to create the table within try block to cath errors assoicated with executing this statemenet 
+
+try {
+    $db->exec($sqlCreateTable);
+    echo "Table 'users' created successfully or already exists.<br>";
+} 
+
+// catch the errors associated with excuting the above statement
+
+
+catch (Exception $e) {
+    echo "Error creating table: " . $e->getMessage() . "<br>";
+}
+
+// get values from form to insert inside the users table
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+echo "<p> $user: $password (only for testing purposes)<p>";
+
+
+// construct an insert query to store values inside table
+$sqlInsert = "INSERT INTO users (user, password) VALUES (:user, :password)";
+
+
+// use try block to construct an insert query  to store values into users.db
+
+try {
+
+    $stmt = $db->prepare($sqlInsert);
+    $stmt->bindValue(':user', $user, SQLITE3_TEXT);
+    $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+    
+    // execute the statement 
+    $stmt->execute();
+    echo " Info iserted: $user and $password values inserted successfully.<br>";
+
+
+} 
+
+// catch and report errors associated with executing above statements
+catch (Exception $e) {
+    echo "Error inserting data: " . $e->getMessage() . "<br>";
+}
+
+
+// if we are done with the database we should close it
+// this allows Apache to use it again quickly, rather than waiting for
+// the database's natural timeout to occur
+
+$db->close();
+unset($db);
+
+
+echo "Database connection closed.<br>";
+
+// include the footer php file which contains the ending of the html commands
+include "footer.php";
 ?>
