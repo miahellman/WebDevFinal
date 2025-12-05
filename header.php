741e0a6a5672722646ded400b711496651e7e30c
jlynli <130990818+jlynli@users.noreply.github.com>
2025-12-05 01:02:51 -0500

    you can click on the logo to go to the home page also fixed links going to the contact page

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <header class="navbar">
-    <div class="logo">SPACE NETWORK</div>
+    <a href="../index.html" class="logo">
+    SPACE NETWORK
+    </a>
     <nav>
       <ul>
         <li><a href="../about.html">ABOUT</a></li>
         <li><a href="../shop/merch.html">MERCH</a></li>
         <li><a href="../games.html">GAMES</a></li>
-        <li><a href="../contact.html">CONTACT</a></li>
+        <li><a href="../contact/contact.html">CONTACT</a></li>
         <li><a href="../loginAndReg/login.html">LOGIN</a></li>
       </ul>
     </nav>
